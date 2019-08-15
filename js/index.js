4609c34a74792760be76326011367b6237f5a2ea
ren-matsushita-swift <ren.git.x@gmail.com>
2019-08-15 16:41:09 +0900

    s

--- js/index.js
+++ js/index.js
@@ -3,15 +3,19 @@ $(function() {
     const view_height = window.innerHeight;
     fab_to_top.hide();
 
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > view_height*2) {
-            fab_to_top.fadeIn();
-        } else {
-            fab_to_top.fadeOut();
-        }
-    });
+    $('#about-data-list').fadeIn();
+    console.log("ffffff");
+    
 
-    fab_to_top.click(function() {
-        $('html,body').animate({scrollTop: 0}, 400, 'swing');
-    });
+    // $(window).scroll(function () {
+    //     if ($(this).scrollTop() > view_height*2) {
+    //         fab_to_top.fadeIn();
+    //     } else {
+    //         fab_to_top.fadeOut();
+    //     }
+    // });
+
+    // fab_to_top.click(function() {
+    //     $('html,body').animate({scrollTop: 0}, 400, 'swing');
+    // });
 });
