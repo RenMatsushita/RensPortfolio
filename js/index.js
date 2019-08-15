6a89c669f5fa1e0cece218eecd9b12de779ac347
ren-matsushita-swift <ren.git.x@gmail.com>
2019-08-15 14:37:30 +0900

    シンプルなパララックスを追加

--- js/index.js
+++ js/index.js
@@ -1,5 +1,17 @@
 $(function() {
-    $('#fab-to-top').click(function() {
+    const fab_to_top = $('#fab-to-top');
+    const view_height = window.innerHeight;
+    fab_to_top.hide();
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > view_height*2) {
+            fab_to_top.fadeIn();
+        } else {
+            fab_to_top.fadeOut();
+        }
+    });
+
+    fab_to_top.click(function() {
         $('html,body').animate({scrollTop: 0}, 400, 'swing');
     });
 });
